3ae3849cf5f00b92537fe2b8438d21409cf4532c
Alex HH <alexhdzherdocia@gmail.com>
2017-03-13 01:46:22 -0700

    added mock slider and mock progress bar with signals

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->progressBar->setEnabled(true);
+    ui->horizontalSlider->setEnabled(true);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <random>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +15,9 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void on_dial_sliderMoved(int position);
 
 private:
     Ui::MainWindow *ui;
